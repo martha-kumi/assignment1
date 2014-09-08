9019ee21fce6d0ac7684e4f0ca0369445c5b72e1
Martha Kumi <martha.kumi@gmail.com>
2014-09-08 00:49:25 +0000

    files for step-6

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,10 +4,10 @@
 
 var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl',['$scope', '$http', function($scope, $http) {
+phonecatApp.controller('PhoneListCtrl', function($scope, $http) {
   $http.get('phones/phones.json').success(function(data) {
-    $scope.phones = data.splice(0, 5);
+    $scope.phones = data;
   });
 
   $scope.orderProp = 'age';
-}]);
+});
