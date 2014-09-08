ed4204618c7d1cf756c3c29f522035c339a0d69b
Martha Kumi <martha.kumi@gmail.com>
2014-09-08 12:22:35 +0000

    initial files for step-0

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,13 +1,3 @@
 'use strict';
 
 /* Controllers */
-
-var phonecatApp = angular.module('phonecatApp', []);
-
-phonecatApp.controller('PhoneListCtrl', function($scope, $http) {
-  $http.get('phones/phones.json').success(function(data) {
-    $scope.phones = data;
-  });
-
-  $scope.orderProp = 'age';
-});
